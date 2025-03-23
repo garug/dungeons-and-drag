2016aa030391c822dba78abcc342002ebf1e3e3d
garug <allefff@gmail.com>
2025-03-22 21:27:01 -0300

    moving character

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,14 +1,19 @@
-extends Node2D
+extends CharacterBody2D
 
 var dragging = false
 
-func _process(delta: float) -> void:
-	if dragging:
-		print(position)
+var speed = 20
+
+func _physics_process(delta: float) -> void:
+	if !dragging:
+		return
+	var direction = get_global_mouse_position() - position
+	velocity = direction * speed
+	move_and_slide()
 
 func _input(event):
 	if dragging and event is InputEventMouseButton and event.pressed == false:
-		dragging = false;
+		dragging = false
 
 func _on_touch_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
